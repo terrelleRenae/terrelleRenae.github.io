236f348ac05b80271240038c0af009366fa069dd
terrelleRenae <terrelle.dupree@inspire35.org>
2025-11-13 22:01:17 +0000

    azxcyt

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,25 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(50, 300, 250, 50, "red");
+    createPlatform(350, 400, 250, 50, "black");
+    createPlatform(900, 400, 250, 50, "yellow");
+    createPlatform(600, 300, 250, 50, "green");
+    createPlatform(1200, 300, 250, 50, "red");
     // TODO 3 - Create Collectables
-
-
-
+    createCollectable("database", 700, 275);
+    createCollectable("database", 1300, 275);
+    createCollectable("database", 100, 275);
     
     // TODO 4 - Create Cannons
-
+        createCannon("top", 450, 1500);
+        createCannon("bottom", 1250, 1500);
+        createCannon("left", 300, 1500);
+        createCannon("right", 150, 1000);
 
     
     
